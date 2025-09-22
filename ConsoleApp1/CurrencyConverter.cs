ecd77289cf01f107a74830edd0209bd8db706128
David <twdeveloper@outlook.com>
2025-09-22 16:23:05 +0800

    [摘要] 新增專案檔案及匯率轉換功能
    [功能] .gitignore：新增忽略Visual Studio相關檔案的設定
    [功能] ConsoleApp1.csproj：新增專案設定檔，包含Newtonsoft.Json的引用
    [功能] CurrencyConverter.cs：新增匯率轉換功能，從API獲取匯率
    [功能] Financy.cs：新增金額分攤計算功能，使用CurrencyConverter進行匯率轉換
    [功能] Program.cs：新增主程式，實現使用者輸入金額及人數並計算每人需支付的金額

---
+++ ConsoleApp1/CurrencyConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class CurrencyConverter
+    {
+        const string API_KEY = "________________________"; //請自行申請
+        public float Convert(string From, string To)
+        {
+            HttpClient hc = new HttpClient();
+            var ret = hc.GetAsync($"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/USD/TWD").Result;
+            var JSON = ret.Content.ReadAsStringAsync().Result;
+
+            dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(JSON);
+            return data.conversion_rate;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Financy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Financy
+    {
+        public double SplitMoney(double USDAmount, int People)
+        {
+            var currencyConverter = new CurrencyConverter();
+            //使用到外部函式(抓取匯率)
+            double rate = currencyConverter.Convert("USD", "TWD");
+            //計算台幣總金額
+            double Total = USDAmount * rate;
+            //回傳一個人需要付多少錢(台幣)
+            return Total / People;
+        }
+    }
+}
